5b58a0a272fcfc3e77f7f7a9f9e19ab33ea7bd20
Eduardo <eduar766gmail.com>
2020-05-31 11:21:54 -0400

    Add skills and Info

--- src/app/models.py
+++ src/app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Description(models.Model):
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text
+
+
+class Skills(models.Model):
+    title = models.CharField(max_length=30)
+    progress = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+    
+
+class Services(models.Model):
+    icon = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.title
--- src/app/views.py
+++ src/app/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from .models import Description, Skills, Services
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    description = Description.objects.get(pk=1)
+    skills = Skills.objects.all()
+    services = Services.objects.all()
+
+    context = {
+        'description': description,
+        'skills': skills,
+        'services': services
+    }
+
+    return render(request, 'index.html', context)
