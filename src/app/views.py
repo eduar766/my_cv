aaaf90eb911944a76fc2e9841e0efbf10f10ad01
Eduardo <eduar766gmail.com>
2020-06-01 23:37:58 -0400

    add medium request to blog

--- src/app/views.py
+++ src/app/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
 from .models import Description, Skills, Services, Portfolio, PortCat, Resume
 
-# Create your views here.
+import requests
+
+def medium_request():
+    url_medium = 'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@eduar766'
+    response = requests.get(url_medium).json()
+    data = []
+    response = response['items']
+    length = len(response)
+    p = 0
+
+    while p < length:
+        collect = {
+            'title': response[p]['title'],
+            'link': response[p]['link'],
+            'pub_date': response[p]['pubDate'],
+            'description': response[p]['description'],
+            'thumbnail': response[p]['thumbnail'],
+        }
+
+        data.append(collect)
+        p = p + 1 
+
+    print(data)
+
+    return data
 
 def index(request):
     description = Description.objects.get(pk=1)
@@ -11,13 +35,16 @@ def index(request):
     p_category = PortCat.objects.all()
     resume = Resume.objects.all().order_by('-pk')
 
+    medium = medium_request()
+
     context = {
         'description': description,
         'skills': skills,
         'services': services,
         'portfolio': portfolio,
         'p_category': p_category,
-        'resume': resume
+        'resume': resume,
+        'medium': medium
     }
 
     return render(request, 'index.html', context)
